c332c36b85e48e373acb28aca60dc0a6447b574b
Rey Beebe <ark2000@foxmail.com>
2023-01-02 13:51:00 +0800

    Better Notification System

--- addons/panku_console/components/resident_logs/log_item.gd
+++ addons/panku_console/components/resident_logs/log_item.gd
@@ -1,5 +1,7 @@
 extends HBoxContainer
 
+var life = 3.0
+
 var amount := 1:
 	set(v):
 		amount = v
@@ -9,25 +11,38 @@ var amount := 1:
 @onready var amount_label:Label = $Amount/MarginContainer/Label
 @onready var amount_panel:PanelContainer = $Amount
 
+@onready var progress_a := $Content/HBoxContainer/A
+@onready var progress_b := $Content/HBoxContainer/B
+
 var tween2:Tween
+var tween3:Tween
 
 func amount_pop_anim():
 	if tween2 and tween2.is_valid():
 		tween2.kill()
-	tween2 = get_tree().create_tween()
+	tween2 = create_tween()
 	tween2.tween_property(amount_panel, "scale", Vector2(1, 1), 0.01)
 	tween2.tween_property(amount_panel, "scale", Vector2(1.2, 1.2), 0.05)
 	tween2.tween_property(amount_panel, "scale", Vector2(1, 1), 0.05)
+
+func fade_out():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.2)
+	tween.tween_callback(queue_free).set_delay(0.2)
+
+func set_progress(v:float):
+	progress_a.size_flags_stretch_ratio = v
+	progress_b.size_flags_stretch_ratio = 1.0 - v
 
 func _ready():
 	content_label.meta_clicked.connect(
 		func(meta):
 			OS.shell_open(str(meta))
 	)
+	modulate.a = 0.0
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1.0, 0.2)
 
-#	await get_tree().process_frame
-#	var tween = get_tree().create_tween()
-#	modulate.a = 0.0
-#	position.x -= size.x / 2
-#	tween.tween_property(self, "position:x", size.x / 2, 0.2).as_relative().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-#	tween.set_parallel().tween_property(self, "modulate:a", 1.0, 0.2).as_relative().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween3 = create_tween()
+	tween3.tween_method(set_progress, 1.0, 0.0, life)
+	tween3.tween_callback(fade_out)
