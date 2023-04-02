1eec0ea8e72999a8b5353621fd914e60bbe6396a
k2kra <ark2000@foxmail.com>
2023-04-02 17:31:57 +0800

    feat: better joystick button
    refers to: https://github.com/Ark2000/PankuConsole/issues/81

--- addons/panku_console/components/exporter/joystick_button.gd
+++ addons/panku_console/components/exporter/joystick_button.gd
@@ -1,9 +1,17 @@
 extends Button
 
-@export var ratio := 0.01
-
 signal value_changed(delta:Vector2)
 
+var prev_mouse_mode
+var prev_mouse_pos
+
 func _gui_input(e:InputEvent):
+	if e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_LEFT and e.pressed:
+		prev_mouse_mode = DisplayServer.mouse_get_mode()
+		prev_mouse_pos = get_viewport().get_mouse_position()
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
 	if e is InputEventMouseMotion and e.button_mask != MOUSE_BUTTON_NONE:
-		value_changed.emit(e.velocity * ratio)
+		value_changed.emit(e.relative)
+	if e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_LEFT and !e.pressed:
+		DisplayServer.mouse_set_mode(prev_mouse_mode)
+		DisplayServer.warp_mouse(prev_mouse_pos)
