eaa13b027490dea9dc62d8206700779821d6ee51
k2kra <ark2000@foxmail.com>
2023-06-29 02:28:00 +0800

    refactor: remove popup menu button

--- addons/panku_console/modules/interactive_shell/input_field/input_area.gd
+++ addons/panku_console/modules/interactive_shell/input_field/input_area.gd
@@ -10,7 +10,6 @@ signal navigate_histories(histories:Array, cur:int)
 
 @export var input:LineEdit
 @export var btn:Button
-@export var menubtn:MenuButton
 
 func _ready():
 	input.text_submitted.connect(
@@ -32,27 +31,4 @@ func _ready():
 		func():
 			if is_visible_in_tree():
 				input.call_deferred("grab_focus")
-	)
-
-	menubtn.mouse_entered.connect(
-		func():
-			menubtn.modulate = Color.GREEN
-	)
-	menubtn.mouse_exited.connect(
-		func():
-			menubtn.modulate = Color.WHITE
-	)
-	menubtn.get_popup().id_pressed.connect(
-		func(id:int):
-			#watch
-			var exp = input.text.lstrip(" ").rstrip(" ")
-			if id == 0:
-				pass
-				# var window = console.add_monitor_window(exp, 0.1)
-				# window.centered()
-			#add button
-			elif id == 1:
-				pass
-				# var window = console.add_monitor_window(exp, 999999)
-				# window.centered()
 	)
