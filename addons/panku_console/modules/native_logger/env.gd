15b5aecd55b6cfb2eb36a5c8684470622f55e7d8
k2kra <ark2000@foxmail.com>
2023-07-08 16:08:46 +0800

    feat: add logger clear command

--- addons/panku_console/modules/native_logger/env.gd
+++ addons/panku_console/modules/native_logger/env.gd
@@ -7,3 +7,7 @@ func open() -> void:
 const _HELP_toggle_overlay = "Toggle visibility of logger overlay"
 func toggle_overlay() -> void:
 	_module.toggle_overlay()
+
+const _HELP_clear = "Clear logs"
+func clear() -> void:
+	_module.logger_ui.clear_all()
