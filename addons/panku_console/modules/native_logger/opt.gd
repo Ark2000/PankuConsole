1eaf10d1645f898d6b3cd18cbb34f35bf39df6ba
Feo Wu <ark2000@foxmail.com>
2023-11-09 19:56:50 +0800

    fix: export enum type 4.2 incompatible

--- addons/panku_console/modules/native_logger/opt.gd
+++ addons/panku_console/modules/native_logger/opt.gd
@@ -22,7 +22,7 @@ func open_window():
 #	get:
 #		return _module.output_overlay.visible
 
-@export var screen_overlay:PankuModuleNativeLogger.ScreenOverlayDisplayMode:
+@export_enum("Always Show", "ShowIfShellVisible", "NeverShow") var screen_overlay:int:
 	set(v):
 		_module.set_overlay_display_mode(v)
 	get:
